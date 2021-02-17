a61a5d6e6fe645bcd72687c96bf9587e507b9d17
EBureacova <ecaterina.bureacova@endava.com>
2021-02-17 16:56:42 +0200

    jenkins file path changed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,11 +2,10 @@ node {
    stage 'Run Load Test'
    echo 'Starting test with Jmeter'
    bat """
-    CALL CD %WORKSPACE%\\bin
-    CALL RD /S /Q %WORKSPACE%\\bin\\BureacovaEntranceTask\\HTML
-    CALL DEL "%WORKSPACE%\\bin\\BureacovaEntranceTask\\log.csv"
-    CALL jmeter.bat -n -t %WORKSPACE%\\bin\\BureacovaEntranceTask\\TestPlanBureacovaUpdate.jmx ^
-        -l %WORKSPACE%\\bin\\BureacovaEntranceTask\\log.csv -e -o %WORKSPACE%\\bin\\BureacovaEntranceTask\\HTML ^
+    CALL RD /S /Q HTML
+    CALL DEL "log.csv"
+    CALL C:\\Users\\EBureacova\\apache-jmeter-5.4.1\\bin\\jmeter.bat -n -t TestPlanBureacovaUpdate.jmx ^
+        -l log.csv -e -o HTML ^
         -Jduration=%duration% -Jusers=%users% -jrampUp=%rampUp% -Jjmeterengine.force.system.exit=true
     """
    echo 'Test completed'

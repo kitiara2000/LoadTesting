8bcd7cd5b76c2ba9f1989cfd6ede7c5a01e91033
EBureacova <ecaterina.bureacova@endava.com>
2021-02-17 16:58:42 +0200

    jenkins file changed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,5 @@
 node {
    stage 'Run Load Test'
    echo 'Starting test with Jmeter'
-   bat """
-    CALL RD /S /Q HTML
-    CALL DEL "log.csv"
-    CALL C:\\Users\\EBureacova\\apache-jmeter-5.4.1\\bin\\jmeter.bat -n -t TestPlanBureacovaUpdate.jmx ^
-        -l log.csv -e -o HTML ^
-        -Jduration=%duration% -Jusers=%users% -jrampUp=%rampUp% -Jjmeterengine.force.system.exit=true
-    """
    echo 'Test completed'
 }
